933ed74c27b680faa82113ec6090646ae2826e3e
KarlBerod <karl.berod@gmail.com>
2024-03-08 04:49:59 +0100

    Compatibilité

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import '../styles/App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+// import Component1 from "./";
+// import Component2 from "./";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <Router>
+      <div>
+        {/* <Routes>
+          <Route path="./" element={<Component1 />} />
+          <Route path="./" element={<Component2 />} />
+        </Routes> */}
+      </div>
+      <p>yo</p>
+    </Router>
   );
 }
 
